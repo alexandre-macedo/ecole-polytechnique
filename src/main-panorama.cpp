42934c16109bb1ed4ee598fa781e2a9d3ab7661d
Alexandre Macedo <arj.macedo@gmail.com>
2018-02-12 19:50:19 +0100

    Final version

--- src/main-panorama.cpp
+++ src/main-panorama.cpp
@@ -24,16 +24,6 @@ int main(int argc, char* argv[]) {
     images1[i] = cv::imread(path);
   }
   buildPanorama(images1, result, detector_type, threshold, n_interaction, s_index, 1);
-
-  cv::Mat result2;
-  n_images = 4;
-  s_index = 14;
-  std::vector<cv::Mat> images2(n_images);
-  for (int i = 0; i < n_images; i++) {
-    std::string path = prefix + std::to_string(s_index + i) + suffix;
-    images2[i] = cv::imread(path);
-  }
-  buildPanorama(images2, result2, detector_type, threshold, n_interaction, s_index, 1);
   
   return 0;
 }
